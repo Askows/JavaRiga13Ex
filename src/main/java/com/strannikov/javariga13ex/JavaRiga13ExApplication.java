cb8920450546706115fdfc2b1c935135ecbf138f
Andrejs Straņņikovs <strannikow.andrew@gmail.com>
2022-04-08 13:19:54 +0300

    first commit

---
+++ src/main/java/com/strannikov/javariga13ex/JavaRiga13ExApplication.java
@@ -0,0 +1,13 @@
+package com.strannikov.javariga13ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRiga13ExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRiga13ExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/strannikov/javariga13ex/JavaRiga13ExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strannikov.javariga13ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRiga13ExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
