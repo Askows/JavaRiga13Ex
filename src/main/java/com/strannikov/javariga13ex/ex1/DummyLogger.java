18eb9c929066501a97ec66d491a63b8583e1cba2
Andrejs Straņņikovs <strannikow.andrew@gmail.com>
2022-04-08 14:25:20 +0300

    Exercise 1 new message with log.info

--- src/main/java/com/strannikov/javariga13ex/ex1/DummyLogger.java
+++ src/main/java/com/strannikov/javariga13ex/ex1/DummyLogger.java
@@ -14,5 +14,7 @@ public class DummyLogger implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Hello from task1");
+        //log use because added Slf4J it better than System.out.println()
+        log.info("Hello from task1");
     }
 }
