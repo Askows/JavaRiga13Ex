d1dbfc61162ce0992e5d2af2238e4d301ae1585b
Andrejs Straņņikovs <strannikow.andrew@gmail.com>
2022-04-09 12:35:49 +0300

    Exercise 4 configuration ready

--- src/main/java/com/strannikov/javariga13ex/ex4/CustomConfiguration.java
+++ src/main/java/com/strannikov/javariga13ex/ex4/CustomConfiguration.java
@@ -1,11 +1,22 @@
 package com.strannikov.javariga13ex.ex4;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CustomConfiguration {
+public class CustomConfiguration  {
 
-
-
+    @Bean
+    public DummyLogger dummyLogger(){
+        return new DummyLogger();
+    }
+    @Bean
+    public ListUtil listUtility(){
+        return new ListUtil();
+    }
+    @Bean("stringUtility ")
+    public StringUtil stringUtil(){
+        return new StringUtil();
+    }
 
 }
