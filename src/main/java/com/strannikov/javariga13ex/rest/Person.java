774e4c4425f97643fd3a9b3fcdad42e7736614da
Andrejs Straņņikovs <strannikow.andrew@gmail.com>
2022-04-11 12:27:37 +0300

    CRUD test api

--- src/main/java/com/strannikov/javariga13ex/rest/Person.java
+++ src/main/java/com/strannikov/javariga13ex/rest/Person.java
@@ -1,10 +1,19 @@
 package com.strannikov.javariga13ex.rest;
 
+import lombok.Data;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 
-@Value
+@Data
 public class Person {
-     String name;
-     int age;
+     private String name;
+     private int age;
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public void setAge(int age) {
+          this.age = age;
+     }
 }
--- src/main/java/com/strannikov/javariga13ex/rest/TestApi.java
+++ src/main/java/com/strannikov/javariga13ex/rest/TestApi.java
@@ -2,18 +2,55 @@ package com.strannikov.javariga13ex.rest;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class TestApi {
+    private List<Person> db = new ArrayList<>();
 
 
+    //GET!!!
     @GetMapping("/hello/{name}")
-    public String sayHello(@PathVariable String name) {
-        return "Hello " + name;
+    public String sayHello(@PathVariable("name") String firstName) {
+        return "Hello " + firstName;
     }
 
+    ///SAVE!
     @PostMapping()
     public Person saveName(@RequestBody Person person) {
-
+        db.add(person);
         return person;
+    }
+
+    // UPDATE!!!
+    @PutMapping("/{name}")
+    public Person updateName(@PathVariable("name") String firstName,
+                             @RequestBody Person person) {
+        for (Person person1 : db) {
+            if (person1.getName().equals(firstName)) {
+                person1.setName(person.getName());
+                person1.setAge(person.getAge());
+
+            }
+        }
+        person.setName(firstName);
+        return person;
+    }
+
+    @GetMapping
+    public List<Person> getAll() {
+        return db;
+    }
+
+    @DeleteMapping("/{name}")
+    public String delete(@PathVariable("name") String firstName) {
+        for (Person person1 : db) {
+            if (person1.getName().equals(firstName)) {
+                db.remove(person1);
+                return "Deleted";
+            }
+        }
+        return "Not deleted";
     }
 }
